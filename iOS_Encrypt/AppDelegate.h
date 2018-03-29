a78a5426b59b565fbb5960e27d0856e7f5d4af36
lkp <mylovelkp@163.com>
2018-03-29 16:05:19 +0800

    上传项目

---
+++ iOS_Encrypt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_Encrypt
+//
+//  Created by lkp on 2018/3/29.
+//  Copyright © 2018年 LKP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_Encrypt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_Encrypt
+//
+//  Created by lkp on 2018/3/29.
+//  Copyright © 2018年 LKP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
